6db9747be1d23fd0bd6c129defbc8a9319bfb477
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-06-22 00:01:18 +0530

    Array in Class

---
+++ program162.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class ArrayX
+{
+  public :
+  int *Arr;
+  int iSize;
+
+  ArrayX(int X)
+  {
+    iSize = X;
+    Arr = new int[iSize];
+  }
+
+  ~ArrayX()
+  {
+    delete []Arr;
+  }
+};
+
+int main()
+{
+  ArrayX *aobj1 = new ArrayX(5);
+
+  // Logic (function call)
+  cout<<aobj1->iSize<<"\n";
+
+  delete aobj1;
+  return 0;
+}
